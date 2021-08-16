19a00e2c15ea7caff4785633790b9ea618e0e09c
tanjim1905 <tanjimahmed1905@gmail.com>
2021-08-16 13:53:17 +0600

    Day 2

--- HackerRank-10-Days-Of-JavaScript.js
+++ HackerRank-10-Days-Of-JavaScript.js
@@ -15,7 +15,8 @@ function greeting(parameterVariable) {
     Day 0: Data Types
    =================== */
 
-   function performOperation(secondInteger, secondDecimal, secondString) {
+function performOperation(secondInteger, secondDecimal,
+    secondString) {
     // Declare a variable named 'firstInteger' and initialize with integer value 4.
     const firstInteger = 4;
     
@@ -35,3 +36,64 @@ function greeting(parameterVariable) {
     
     // Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The        variable 'firstString' must be printed first.
 }
+
+/* ============================
+    Day 1: Arithmetic Operators
+   ============================ */
+
+   function getArea(length, width) {
+    let area;
+    // Write your code here
+    area = length * width;
+    return area;
+}
+
+/**
+*   Calculate the perimeter of a rectangle.
+*	
+*	length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the perimeter of a rectangle.
+**/
+function getPerimeter(length, width) {
+    let perimeter;
+    // Write your code here
+    perimeter = 2 * (length + width);
+    return perimeter;
+}
+
+/* ==================
+    Day 1: Functions
+   ================== */
+
+
+/*
+ * Create the function factorial here
+ */
+function factorial(n){
+    let fact = 1;
+    for(let i = 1; i <= n; i++){
+        fact *= i;
+    }
+    return fact;
+}
+
+/* =====================
+    Day 1: Let and Const
+   ===================== */
+
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    
+    let r = readLine()
+    const PI = Math.PI;
+    // Print the area of the circle:
+    let area = PI * (r * r);
+    console.log(area);
+    // Print the perimeter of the circle:
+    let perimeter = 2 * PI * r;
+    console.log(perimeter);
+}
+
+
